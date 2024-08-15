4aae33a993073e32aeca201316bf125b00fd7bc0
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-08-14 23:58:48 -0300

    Update

--- update_srv1.sh
+++ update_srv1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Funcionando SSORI SRV1"
+apt update && apt install isc-dhcp-server
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/dhcpd.conf -O /etc/dhcp/dhcpd.conf --no-checkcertificates --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/isc-dhcp-sever -O /etc/default/isc-dhcp-server --no-checkcertificates --no-cache
+systemctl restart isc-dhcp-server
