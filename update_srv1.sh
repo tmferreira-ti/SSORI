6b96e1f49d0195e86feabd6b97867951287d1b51
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-08-15 00:00:49 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update && apt install isc-dhcp-server
-wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/dhcpd.conf -O /etc/dhcp/dhcpd.conf --no-checkcertificates --no-cache
-wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/isc-dhcp-sever -O /etc/default/isc-dhcp-server --no-checkcertificates --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/dhcpd.conf -O /etc/dhcp/dhcpd.conf --no-check-certificate --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/isc-dhcp-sever -O /etc/default/isc-dhcp-server --no-check-certificate --no-cache
 systemctl restart isc-dhcp-server
