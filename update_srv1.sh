b028cf4a90f8a1dae52b002caa7dc1bfd73b0601
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-08-29 08:49:44 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -7,6 +7,7 @@
 wget https://github.com/tmferreira-ti/SSORI/raw/main/sysctl.conf -O /etc/sysctl.conf -no-check-certificate --no-cache
 wget https://github.com/tmferreira-ti/SSORI/blob/main/configs/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
 wget https://github.com/tmferreira-ti/SSORI/raw/main/configs/nftables.conf -O /etc/nftables.conf --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/SSORI/raw/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 
 systemctl enable nftables
 reboot
