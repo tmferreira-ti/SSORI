184723185e0dc0c216aa083b662e722ef09c070e
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-09-02 11:25:43 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -4,7 +4,7 @@
 #wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/isc-dhcp-sever -O /etc/default/isc-dhcp-server --no-check-certificate --no-cache
 #systemctl restart isc-dhcp-server
 
-wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/sysctl.conf -O /etc/sysctl.conf -no-check-certificate --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/sysctl.conf -O /etc/sysctl.conf --no-check-certificate --no-cache
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/configs/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/configs/nftables.conf -O /etc/nftables.conf --no-check-certificate --no-cache
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
